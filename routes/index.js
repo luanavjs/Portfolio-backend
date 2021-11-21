bd34cc7ed2a0a885519f58ef363365cfdd2a061b
Luana <luanalorenavallejos@gmail.com>
2021-11-20 22:49:47 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+module.exports = router;*/
+
+var express = require('express');
+var router = express.Router();
+const Profile = require("../models/profiles");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const profiles = await Profiles.find();
+  res.render('index', { title: 'Express', profiles });
+});
+
 module.exports = router;
+
